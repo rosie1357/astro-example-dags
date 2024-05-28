91bef80ba6f81fb6aaef0437d6b97f69497fe5f8
Rosie Malsberger <rosie.malsberger@definitivehc.com>
2024-05-28 11:44:52 -0400

    test: rename tag

--- dags/example_buns.py
+++ dags/example_buns.py
@@ -1,7 +1,6 @@
-from airflow import Dataset
-from airflow.decorators import dag, task
+from airflow.decorators import task, dag
+import json
 from pendulum import datetime
-import requests
 
 #Define the basic parameters of the DAG, like schedule and start_date
 @dag(
@@ -9,49 +8,39 @@ import requests
     schedule="@daily",
     catchup=False,
     doc_md=__doc__,
-    default_args={"owner": "Astro", "retries": 3},
-    tags=["example"],
+    default_args={"owner": "Rosie"},
+    tags=["charlie"],
 )
-def example_buns():
-    #Define tasks
-    @task(
-        #Define a dataset outlet for the task. This can be used to schedule downstream DAGs when this task has run.
-        outlets=[Dataset("current_astronauts")]
-    )  # Define that this task updates the `current_astronauts` Dataset
-    def get_astronauts(**context) -> list[dict]:
-        """
-        This task uses the requests library to retrieve a list of Astronauts 
-        currently in space. The results are pushed to XCom with a specific key
-        so they can be used in a downstream pipeline. The task returns a list
-        of Astronauts to be used in the next task.
-        """
-        r = requests.get("http://api.open-notify.org/astros.json")
-        number_of_people_in_space = r.json()["number"]
-        list_of_people_in_space = r.json()["people"]
+def myfirstdag():
 
-        context["ti"].xcom_push(
-            key="number_of_people_in_space", value=number_of_people_in_space
-        )
-        return list_of_people_in_space
+    @task()  # Define that this task updates the `current_astronauts` Dataset
+    def extract():
+
+        data = {'carlos': 3, 'dots': 6, 'lulu': 2, 'gigi': 2}
+
+        data_dict = json.loads(data)
+        print(data_dict)
+        
+        return data_dict
 
     @task
-    def print_astronaut_craft(greeting: str, person_in_space: dict) -> None:
-        """
-        This task creates a print statement with the name of an 
-        Astronaut in space and the craft they are flying on from 
-        the API request results of the previous task, along with a 
-        greeting which is hard-coded in this example.
-        """
-        craft = person_in_space["craft"]
-        name = person_in_space["name"]
+    def transform(data_dict):
+        
+        total_age = 0
 
-        print(f"{name} is currently in space flying on the {craft}! {greeting}")
+        for v in data_dict.values():
+            total_age += v
 
-    #Use dynamic task mapping to run the print_astronaut_craft task for each 
-    #Astronaut in space
-    print_astronaut_craft.partial(greeting="Hello! :)").expand(
-        person_in_space=get_astronauts() #Define dependencies using TaskFlow API syntax
-    )
+        return total_age
+
+    @task
+    def load(total_age):
+
+        print(total_age)
+
+    create_bunnies = extract()
+    sum_ages = transform(create_bunnies)
+    load(sum_ages())
 
 #Instantiate the DAG
-example_buns()
+myfirstdag()
